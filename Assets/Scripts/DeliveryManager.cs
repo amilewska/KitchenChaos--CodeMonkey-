ec4cea41439d93bc59962e156a3d5266e6a37d93
amilewska <adoryanna@gmail.com>
2023-03-28 14:36:42 +0200

    ui recipe

---
+++ Assets/Scripts/DeliveryManager.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DeliveryManager : MonoBehaviour
+{
+
+    public event EventHandler OnRecipeSpawned;
+    public event EventHandler OnRecipeCompleted;
+
+    public static DeliveryManager Instance { get; private set; }
+    [SerializeField] private RecipeListSO recipeListSO;
+    private List<RecipeSO> waitingRecipeSOList;
+    private float spawnRecipeTimer;
+    private float spawnRecipeTimerMax = 4f;
+    private int waitingRecipesMax = 4;
+
+    private void Awake()
+    {
+        Instance = this;
+        waitingRecipeSOList = new List<RecipeSO>();
+    }
+
+    private void Update()
+    {
+        spawnRecipeTimer -= Time.deltaTime;
+        if (spawnRecipeTimer <= 0)
+        {
+            spawnRecipeTimer = spawnRecipeTimerMax;
+
+            if(waitingRecipeSOList.Count < waitingRecipesMax)
+            {
+
+                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[UnityEngine.Random.Range(0, recipeListSO.recipeSOList.Count)];
+
+                Debug.Log(waitingRecipeSO.recipeName);
+
+                waitingRecipeSOList.Add(waitingRecipeSO);
+
+                OnRecipeSpawned?.Invoke(this, EventArgs.Empty);
+            }
+
+            
+        }
+    }
+
+
+    public void DeliverRecipe(PlateKitchenObject plateKitchenObject)
+    {
+        for (int i = 0; i < waitingRecipeSOList.Count; i++)
+        {
+            RecipeSO waitingRecipeSO = waitingRecipeSOList[i];
+            if(waitingRecipeSO.kitchenObjectSOList.Count == plateKitchenObject.GetKitchenObjectSOList().Count)
+            {
+                //Has the same number of ingredients
+                bool plateContentsMatchesRecipes = true;
+                foreach(KitchenObjectSO recipeKitchenObjectSO in waitingRecipeSO.kitchenObjectSOList)
+                {
+                    //Cycling through all ingredients in recipe
+                    bool ingredientFound = false;
+                    foreach(KitchenObjectSO plateKitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList())
+                    {
+                        //cycling through all ingredient in the plate
+                        if(plateKitchenObjectSO == recipeKitchenObjectSO)
+                        {
+                            //ingredient matches!
+                            ingredientFound = true;
+                            break;
+                        }
+                    }
+
+                    if (!ingredientFound)
+                    {
+                        //this recipe ingredient was not found on the plate
+                        plateContentsMatchesRecipes = false;
+                    }
+                }
+
+                if (plateContentsMatchesRecipes)
+                {
+                    //player delivered the correct recipe
+                    waitingRecipeSOList.RemoveAt(i);
+
+                    OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
+
+                    return;
+                }
+            }
+
+        }
+
+        //no matches found
+        //player did not deliver correct recipe
+
+
+
+    }
+
+    public List<RecipeSO> GetWaitingRecipeSOList()
+    {
+        return waitingRecipeSOList;
+    }
+
+}
---
+++ Assets/Scripts/UI/DeliveryManagerUI.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryManagerUI : MonoBehaviour
+{
+    [SerializeField] private Transform containter;
+    [SerializeField] private Transform recipeTemplate;
+
+    private void Awake()
+    {
+        recipeTemplate.gameObject.SetActive(false);
+
+    }
+    private void Start()
+    {
+        DeliveryManager.Instance.OnRecipeSpawned += DeliveryManager_OnRecipeSpawned;
+        DeliveryManager.Instance.OnRecipeCompleted += DeliveryManager_OnRecipeCompleted;
+        UpdateVisual();
+    }
+
+    private void DeliveryManager_OnRecipeCompleted(object sender, System.EventArgs e)
+    {
+        UpdateVisual();
+    }
+
+    private void DeliveryManager_OnRecipeSpawned(object sender, System.EventArgs e)
+    {
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        foreach(Transform child in containter)
+        {
+            if (child == recipeTemplate) continue;
+            Destroy(child.gameObject);
+
+        }
+
+       foreach(RecipeSO recipeSO in DeliveryManager.Instance.GetWaitingRecipeSOList())
+        {
+            Transform recipeTransform = Instantiate(recipeTemplate, containter);
+            recipeTransform.gameObject.SetActive(true);
+            recipeTransform.GetComponent<DeliveryManagerSignleUI>().SetRecipeSO(recipeSO);
+        }
+
+    }
+
+}
