531f8b74135b736b24224d45f421f4a6a22c9d33
amilewska <adoryanna@gmail.com>
2023-04-13 20:33:01 +0200

    ui

--- Assets/Scripts/UI/OptionsUI.cs
+++ Assets/Scripts/UI/OptionsUI.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class OptionsUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static OptionsUI Instance { get; private set; }
+
+    [SerializeField] private Button soundEffetcsButton;
+    [SerializeField] private Button musicButton;
+    [SerializeField] private Button closeButton;
+    [SerializeField] private TextMeshProUGUI soundEffectsText;
+    [SerializeField] private TextMeshProUGUI musicText;
+
+    private void Awake()
     {
-        
+        Instance = this;
+
+        soundEffetcsButton.onClick.AddListener(() =>
+        {
+            SoundManager.Instance.ChagneVolume();
+            UpdateVisual();
+        });
+
+        musicButton.onClick.AddListener(() =>
+        {
+            MusicManager.Instance.ChagneVolume();
+            UpdateVisual();
+
+        });
+
+        closeButton.onClick.AddListener(() =>
+        {
+            Hide();
+
+        });
+    }
+    private void Start()
+    {
+        GameManager.Instance.OnGameUnpaused += GameManager_OnGameUnpaused;
+        UpdateVisual();
+        Hide();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GameManager_OnGameUnpaused(object sender, System.EventArgs e)
     {
-        
+        Hide();
     }
+
+    private void UpdateVisual()
+    {
+        soundEffectsText.text = "Sound Effects: " + Mathf.Round(SoundManager.Instance.GetVolume()*10).ToString("#.#");
+        musicText.text = "Music: " + Mathf.Round(MusicManager.Instance.GetVolume() * 10).ToString();
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
 }
